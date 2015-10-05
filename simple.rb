837a5d1c0a87f6b2a0826997b6bca3dffb9b2288
rodinux <rodolphe.robles@sfr.fr>
2015-10-05 13:37:06 +0200

    suppression de fichier inutile, réédition du readme, changement dans le css, plus clairet plus responsive

--- simple.rb
+++ simple.rb
@@ -14,8 +14,9 @@ end
 get '/diplome' do
 	erb :diplome
 end
+
 get '/proximite' do
-           erb :proximite
+      erb :proximite
 end
 
 get '/terroir' do
@@ -34,47 +35,12 @@ get '/qualite' do
 	erb :qualite
 end
 
-get '/toilettes' do
-   erb :toilettes
+get '/CACES'do
+  erb :CACES
 end
 
-
-
-get '/ecran_noir' do
-	erb :ecran_noir
+get '/filiere' do
+   erb :filiere
 end
 
-get '/caffouilli' do
-	erb :caffouilli
-end
-
-get '/console' do
-            erb :console
-end
-
-get '/fonctionnel' do
-            erb :fonctionnel
-end
-
-get '/formater' do
-            erb :formater
-end
-
-get '/lire' do
-            erb :lire
-end
-
-get '/session' do
-	erb :session
-end
-
-get '/form' do
-           erb :form
-end
-
-post '/form' do
-  @name = "#{params[:post][:first_name]} #{params[:post][:last_name]}"
-  @title = "Hello #{@name}"
-  erb :next
-end
 
